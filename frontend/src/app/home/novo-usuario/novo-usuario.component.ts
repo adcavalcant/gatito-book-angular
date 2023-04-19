fa6ee0176521c9ccd8e2f3c0642fc3c61a023fd8
adcavalcant <aderc19@gmail.com>
2023-04-19 10:00:14 -0400

    Criando componente de Cadastro de Usuário

---
+++ frontend/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css'],
+})
+export class NovoUsuarioComponent {
+  email = '';
+  nome = '';
+  usuario = '';
+  senha = '';
+  criarNovoUsuario() {}
+}
